024b93b348a08ed1a7539adac7c6d206b9147e8d
Ramesh Ganesan <ramesharavinth@gmail.com>
2018-09-30 19:17:09 +0530

    Revert "Initial commit"
    This reverts commit 5f9031908ff76584f6ce1cfbe636ae90ef58d5a1.

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-# a simple for docker 
-FROM python:2.7
-
-ADD python_docker.py /
-
-RUN pip install requests
-
-CMD [ "python", "./python_docker.py" ]
--- python_docker.py
+++
@@ -1,7 +0,0 @@
-import requests
-city        = "Los Angeles"
-
-print("I am from %s" %city)
-
-r = requests.get('http://www.google.com')
-print(r.status_code)
